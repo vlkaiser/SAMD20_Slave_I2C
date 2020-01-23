e11af2dc144047eb694f1a6bf2c2a0bc87a0c67f
Vicki <vlkaiser9@gmail.com>
2020-01-23 12:50:14 -0500

    Updated: Some reg to SAMD20 specific, added comments. Compiles, not working as intended

--- SAMD20_I2C_SL/src/main.c
+++ SAMD20_I2C_SL/src/main.c
@@ -35,9 +35,7 @@
 
 
 /* GLOBALS */
-uint8_t i;
-volatile bool tx_done = false, rx_done = false;
-uint8_t tx_buf[BUF_SIZE] = {1, 2, 3};
+uint8_t i = 0;
 uint8_t rx_buf[BUF_SIZE];
 
 
@@ -48,25 +46,83 @@ void i2c_slave_init(void);
 
 static void pin_set_peripheral_function(uint32_t pinmux);
 
-
-
 /******************************************************************************************************
- * @fn					- i2c_clock_init
- * @brief				- Configure peripheral bus clock (APB) and generic clock for i2c SERCOM module
+ * @fn					- SERCOM2_Handler
+ * @brief				- After transmitting address to slave and receiving ACK/NACK
  * @param[in]			- void
  * @return				- void
  *
- * @note				- 
+ * @note				- Interrupt handler during while(tx_done == true) in master_transaction
+ *						- Overrides weak definition
  ******************************************************************************************************/
-void i2c_clock_init()
+void SERCOM2_Handler(void)
 {
-	struct system_gclk_chan_config gclk_chan_conf;		//struct to configure generic clock for SERCOM
-	uint32_t gclk_index = SERCOM2_GCLK_ID_CORE;
+	/* Check for Address match interrupt */
+	if(SERCOM2->I2CS.INTFLAG.bit.AMATCH)
+	{
+		/* clearing the Address match interrupt */
+		SERCOM2->I2CS.INTFLAG.bit.AMATCH = 1;
+	}
 
-	system_apb_clock_set_mask(SYSTEM_CLOCK_APB_APBC, PM_APBCMASK_SERCOM2);	//Turn on module in Power Manager - peripheral bus C
-	system_gclk_chan_get_config_defaults((&gclk_chan_conf));				//Turn on generic clock for i2c: Default is generator0
-	system_gclk_chan_set_config(gclk_index, &gclk_chan_conf);				//Write defaults to SERCOM2
-	system_gclk_chan_enable(gclk_index);									//Enable
+	/* Data Ready interrupt check */
+	//ToDo: Errata DS80000747B writing CTRLB in DRDY or AMATCH interrupts
+	if(SERCOM2->I2CS.INTFLAG.bit.DRDY)
+	{
+		/* Checking for direction,
+		DIR - 0 for slave read,
+		DIR - 1 for slave write */
+		if (SERCOM2->I2CS.STATUS.bit.DIR)
+		{
+			/* Slave write */
+			/* Is this the last byte to write? */
+			if (i == (BUF_SIZE-1))
+			{
+				SERCOM2->I2CS.DATA.reg = rx_buf[i++];
+				
+				/* wait for stop condition */
+				SERCOM2->I2CS.CTRLB.bit.CMD = 0x2;
+				i = 0;
+			}
+			else
+			{
+				SERCOM2->I2CS.DATA.reg = rx_buf[i++];
+				
+				/* Execute a byte read operation followed by ACK/NACK reception by master*/
+				SERCOM2->I2CS.CTRLB.bit.CMD = 0x3;
+			}
+		}
+		else
+		{
+			/* Slave read */
+			/* Is this the last byte?  Read it and set the Stop command */
+			if (i == (BUF_SIZE-1))
+			{
+				SERCOM2->I2CS.CTRLB.bit.ACKACT = 0;
+				
+				/* Execute acknowledge action succeeded by waiting for any start (S/Sr) condition */
+				SERCOM2->I2CS.CTRLB.bit.CMD = 0x2;
+			}
+			/* Otherwise set for another read byte */
+			else
+			{
+				rx_buf[i++] = SERCOM2->I2CS.DATA.reg;
+				SERCOM2->I2CS.CTRLB.bit.ACKACT = 0;
+				/* Execute acknowledge action succeeded by reception of next byte to master*/
+				SERCOM2->I2CS.CTRLB.bit.CMD = 0x3;
+			}
+		}
+	}
+	if (SERCOM2->I2CS.INTFLAG.bit.PREC)
+	{
+		SERCOM2->I2CS.INTFLAG.bit.PREC = 1;
+
+		if (!SERCOM2->I2CS.STATUS.bit.DIR)
+		{
+			rx_buf[i++] = SERCOM2->I2CS.DATA.reg;
+		}
+
+		i = 0;
+	}
 }
 
 /******************************************************************************************************
@@ -86,6 +142,26 @@ static void pin_set_peripheral_function(uint32_t pinmux)
 	PORT->Group[port].PMUX[((pinmux >> 16) - (port*32))/2].reg &= ~(0xF << (4 * ((pinmux >> 16) & 0x01u)));
 	PORT->Group[port].PMUX[((pinmux >> 16) - (port*32))/2].reg |= (uint8_t)((pinmux &0x0000FFFF) << (4 * ((pinmux >> 16) & 0x01u)));
 
+}
+
+/******************************************************************************************************
+ * @fn					- i2c_clock_init
+ * @brief				- Configure peripheral bus clock (APB) and generic clock for i2c SERCOM module
+ * @param[in]			- void
+ * @return				- void
+ *
+ * @note				- 
+ ******************************************************************************************************/
+void i2c_clock_init()
+{
+	struct system_gclk_chan_config gclk_chan_conf;		//struct to configure generic clock for SERCOM
+	uint32_t gclk_index = SERCOM2_GCLK_ID_CORE;
+
+	system_apb_clock_set_mask(SYSTEM_CLOCK_APB_APBC, PM_APBCMASK_SERCOM2);	//Turn on module in Power Manager - peripheral bus C
+	system_gclk_chan_get_config_defaults(&gclk_chan_conf);					//Turn on generic clock for i2c: Default is generator0
+
+	system_gclk_chan_set_config(gclk_index, &gclk_chan_conf);				//Write defaults to SERCOM2
+	system_gclk_chan_enable(gclk_index);									//Enable
 }
 
 /******************************************************************************************************
@@ -114,21 +190,33 @@ void i2c_pin_init()
 void i2c_slave_init()
 {
 	/* Configurations while I2C is DISABLED: */
+	SERCOM2->I2CS.CTRLA.reg &= ~SERCOM_I2CS_CTRLA_ENABLE;
 
 	/* Configure SERCOM_I2CM hardware register CTRLA:
+	* SPEED does not exist in the SAMD20
 	*	- SDAHOLD bit field as 0x02, SDA hold time is configured for 300-600ns
 	*	- RUNSTDBY bit as 0x01, Generic clock is enabled in all sleep modes (any interrupt can wake up the device)
 	*	- MODE bitfield to 0x4, SERCOM2 is configured as I2C Slave */
 
-		SERCOM2->I2CS.CTRLA.reg = SERCOM_I2CS_CTRLA_SDAHOLD(0x2)	|
-		SERCOM_I2CS_CTRLA_RUNSTDBY									|
-		SERCOM_I2CS_CTRLA_MODE_I2C_SLAVE;
-
+		SERCOM2->I2CS.CTRLA.reg =	SERCOM_I2CS_CTRLA_SDAHOLD(0x2)	|
+									SERCOM_I2CS_CTRLA_RUNSTDBY		|
+									SERCOM_I2CS_CTRLA_MODE_I2C_SLAVE;
+	
+		
+		
 	/* Enable Smart Mode - Will ACK when DATA.DATA is read*/
 	SERCOM2->I2CS.CTRLB.reg = SERCOM_I2CS_CTRLB_SMEN;
 
+	/* Configure Address match configuration? (Datasheet) */
+	//this cleared ACKACT and SMEN???
+	SERCOM2->I2CS.CTRLB.reg = SERCOM_I2CS_CTRLB_AMODE(0x0);		//Respond to Address in addressmask
+
 	/* Write the slave address into ADDR register */
-	SERCOM2->I2CS.ADDR.reg = SLAVE_ADDR << 1 ;
+	SERCOM2->I2CS.ADDR.reg = (SLAVE_ADDR << 1) ;
+	
+	/* Write the slave address into ADDR MASK register (datasheet?) */
+	//SERCOM2->I2CS.ADDR.reg = SLAVE_ADDR << 17 ;
+
 
 	/* Address match interrupt; Data ready interrupt; stop received interrupts are enabled */
 	SERCOM2->I2CS.INTENSET.reg = SERCOM_I2CS_INTENSET_PREC | SERCOM_I2CS_INTENSET_AMATCH | SERCOM_I2CS_INTENSET_DRDY;
@@ -137,84 +225,12 @@ void i2c_slave_init()
 	SERCOM2->I2CS.CTRLA.reg |= SERCOM_I2CS_CTRLA_ENABLE;
 
 	/* SERCOM enable synchronization busy */
-	while(SERCOM2->I2CS.STATUS.bit.SYNCBUSY  & SERCOM_I2CM_STATUS_SYNCBUSY);	//Todo: TEST
+	while(SERCOM2->I2CS.STATUS.bit.SYNCBUSY & SERCOM_I2CM_STATUS_SYNCBUSY);	//Todo: TEST
 
 	/* SERCOM2 handler enabled */
 	system_interrupt_enable(SERCOM2_IRQn);
 
 }
-
-/******************************************************************************************************
- * @fn					- SERCOM2_Handler
- * @brief				- After transmitting address to slave and receiving ACK/NACK
- * @param[in]			- void
- * @return				- void
- *
- * @note				- Interrupt handler during while(tx_done == true) in master_transaction
- *						- Overrides weak definition
- ******************************************************************************************************/
-void SERCOM2_Handler(void)
-{
-	/* Check for Address match interrupt */
-	if(SERCOM2->I2CS.INTFLAG.bit.AMATCH)
-	{
-		/* clearing the Address match interrupt */
-		SERCOM2->I2CS.INTFLAG.bit.AMATCH = 1;
-	}
-	/* Data Ready interrupt check */
-	//ToDo: Errata DS80000747B writing CTRLB in DRDY or AMATCH interrupts
-	if(SERCOM2->I2CS.INTFLAG.bit.DRDY)
-	{
-		/* Checking for direction,
-		DIR - 0 for slave read,
-		DIR - 1 for slave write */
-		if (SERCOM2->I2CS.STATUS.bit.DIR)
-		{
-			/* Slave write */
-			if (i == (BUF_SIZE-1))
-			{
-				SERCOM2->I2CS.DATA.reg = rx_buf[i++];
-				/* wait for stop condition */
-				SERCOM2->I2CS.CTRLB.bit.CMD = 0x2;
-				i = 0;
-			}
-			else
-			{
-				SERCOM2->I2CS.DATA.reg = rx_buf[i++];
-				/* Execute a byte read operation followed by ACK/NACK reception by master*/
-				SERCOM2->I2CS.CTRLB.bit.CMD = 0x3;
-			}
-		}
-		else
-		{
-			/* Slave read */
-			if (i == (BUF_SIZE-1))
-			{
-				SERCOM2->I2CS.CTRLB.bit.ACKACT = 0;
-				/* Execute acknowledge action succeeded by waiting for any start (S/Sr) condition */
-				SERCOM2->I2CS.CTRLB.bit.CMD = 0x2;
-			}
-			else
-			{
-				rx_buf[i++] = SERCOM2->I2CS.DATA.reg;
-				SERCOM2->I2CS.CTRLB.bit.ACKACT = 0;
-				/* Execute acknowledge action succeeded by reception of next byte to master*/
-				SERCOM2->I2CS.CTRLB.bit.CMD = 0x3;
-			}
-		}
-	}
-	if (SERCOM2->I2CS.INTFLAG.bit.PREC)
-	{
-		SERCOM2->I2CS.INTFLAG.bit.PREC = 1;
-		if (!SERCOM2->I2CS.STATUS.bit.DIR)
-		{
-			rx_buf[i++] = SERCOM2->I2CS.DATA.reg;
-		}
-		i = 0;
-	}
-}
-
-
 
 /******************************************************************************************************
  * @fn					- MAIN
@@ -230,8 +246,6 @@ int main (void)
 	i2c_clock_init();
 	i2c_pin_init();
 	i2c_slave_init();
-
-	
 
 	/* This skeleton code simply sets the LED to the state of the button. */
 	while (1) {
